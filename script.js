f184e21f4c384b2b6fd9d7e1a3d0dc7fcf065bab
lescratheurjar <137925663+lescratheurjar@users.noreply.github.com>
2025-05-01 09:14:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,29 @@
-// Liste des fichiers SB3 connus dans le dossier (automatique statique)
-const sb3Files = [
-  "Particle Accelerator.sb3",
-  "jeu2.sb3",
-  "autre.sb3"
-];
-
-// Générer une liste de jeux avec noms lisibles
-const games = sb3Files.map(file => {
-  return {
-    name: file.replace(".sb3", "").replace(/_/g, " "),
-    url: `https://votre-domaine.netlify.app/${file}`
-  };
-});
-
 const selector = document.getElementById("gameSelector");
 const frame = document.getElementById("gameFrame");
 
-function loadGame(url) {
-  frame.src = `https://turbowarp.org/editor?project=${encodeURIComponent(url)}&fullscreen`;
-}
+fetch("games.json")
+  .then(res => res.json())
+  .then(files => {
+    const games = files.map(file => ({
+      name: file.replace(".sb3", "").replace(/_/g, " "),
+      url: `${file}`
+    }));
 
-games.forEach(game => {
-  const option = document.createElement("option");
-  option.value = game.url;
-  option.textContent = game.name;
-  selector.appendChild(option);
-});
+    games.forEach(game => {
+      const option = document.createElement("option");
+      option.value = game.url;
+      option.textContent = game.name;
+      selector.appendChild(option);
+    });
 
-selector.addEventListener("change", e => {
-  loadGame(e.target.value);
-});
+    function loadGame(url) {
+      const fullUrl = `${location.origin}/${url}`;
+      frame.src = `https://turbowarp.org/editor?project=${encodeURIComponent(fullUrl)}&fullscreen`;
+    }
 
-loadGame(games[0].url);
+    selector.addEventListener("change", e => {
+      loadGame(e.target.value);
+    });
+
+    if (games.length > 0) loadGame(games[0].url);
+  });
