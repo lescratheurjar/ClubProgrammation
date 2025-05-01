5109de2bc15d613dcc8e02398844279b3c39a7a4
lescratheurjar <137925663+lescratheurjar@users.noreply.github.com>
2025-05-01 09:22:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,27 +3,22 @@ const frame = document.getElementById("gameFrame");
 
 fetch("games.json")
   .then(res => res.json())
-  .then(files => {
-    const games = files.map(file => ({
-      name: file.replace(".sb3", "").replace(/_/g, " "),
-      url: `${location.origin}/${file}`
-    }));
-
+  .then(games => {
     games.forEach(game => {
       const option = document.createElement("option");
-      option.value = game.url;
+      option.value = game.id;
       option.textContent = game.name;
       selector.appendChild(option);
     });
 
-    function loadGame(projectUrl) {
-      const encodedUrl = encodeURIComponent(projectUrl);
-      frame.src = `https://turbowarp.org/embed.html?project_url=${encodedUrl}`;
+    function loadGame(id) {
+      frame.src = `https://turbowarp.org/${id}/embed?autoplay&fullscreen`;
     }
 
-    selector.addEventListener("change", e => {
-      loadGame(e.target.value);
-    });
+    selector.addEventListener("change", e => loadGame(e.target.value));
 
-    if (games.length > 0) loadGame(games[0].url);
+    if (games.length > 0) {
+      selector.value = games[0].id;
+      loadGame(games[0].id);
+    }
   });
